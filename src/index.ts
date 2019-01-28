c62a57cf4a6c12aba801c92f621b6012cd5bd522
Lukas Horak <horaklukas@centrum.cz>
2019-01-28 16:25:10 +0100

    Export actions from package

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as actionTypes from './services/actionTypes';
 
 export * from './components';
 export * from './HOC';
+export { default as actions } from './services/actions';
 
 export { actionTypes }
