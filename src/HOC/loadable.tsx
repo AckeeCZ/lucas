cb41c1ddb86b7e7676da41c949c4b3af92ab72eb
Lukas Horak <horaklukas@centrum.cz>
2019-07-08 16:05:16 +0200

    Remove unused import

--- src/HOC/loadable.tsx
+++ src/HOC/loadable.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import getDisplayName from 'react-display-name';
-import { omit } from 'lodash';
 
 interface LoaderComponentProps {
     show: boolean;
